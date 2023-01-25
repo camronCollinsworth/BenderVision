074e69e3ec887ad62f4fa4b25dd4598f0c63ceca
camronCollinsworth <camroncollinswor@u.boisestate.edu>
2023-01-24 17:34:54 -0700

    cleanup

--- yolov5/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# run using webcame, results are streamed to the screen, but not saved. Disable --nosave to save results
-#python detect.py --weights runs/train/yolov5s_results/weights/best.pt --img 416 --conf 0.1 --source 0 --view-img --nosave
-
-# run using a test image save to runs folder
-python detect.py --weights runs/train/yolov5s_results/weights/best.pt --img 416 --conf 0.1 --source test2.png
-
-
