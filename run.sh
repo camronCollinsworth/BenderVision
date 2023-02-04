d65c5b07523fce8de062f53e5189e0e28248707b
camronCollinsworth <77373781+camronCollinsworth@users.noreply.github.com>
2023-02-04 15:32:56 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@
 #python detect.py --weights runs/train/yolov5s_results/weights/best.pt --img 416 --conf 0.1 --source 0 --view-img --nosave
 
 # run using a test image save to runs folder
-python detect.py --weights runs/train/yolov5s_results/weights/best.pt --img 416 --conf 0.1 --source test2.png
+python3 detect.py --weights runs/train/yolov5s_results/weights/best.pt --img 416 --conf 0.1 --source test2.png
 
 
