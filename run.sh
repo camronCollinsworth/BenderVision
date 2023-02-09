6c5a42c1e0d3c0e29a22d790fab9b06b58031661
camronCollinsworth <77373781+camronCollinsworth@users.noreply.github.com>
2023-02-08 20:21:50 -0700

    Curse You Pythob3

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # run using webcame, results are streamed to the screen, but not saved. Disable --nosave to save results
-#python detect.py --weights runs/train/yolov5s_results/weights/best.pt --img 416 --conf 0.1 --source 0 --view-img --nosave
+#python3 detect.py --weights runs/train/yolov5s_results/weights/best.pt --img 416 --conf 0.1 --source 0 --view-img --nosave
 
 # run using a test image save to runs folder
 python3 detect.py --weights runs/train/yolov5s_results/weights/best.pt --img 416 --conf 0.1 --source test2.png
